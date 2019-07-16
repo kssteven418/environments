82f09204f9d14e6a6f3ddb15adfd6e5e4bd04672
Sehoon <kssteven418@gmail.com>
2019-07-16 05:45:40 +0000

    change

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+cp .vimrc ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && ./install.sh
+cd .. && remove -rf environments
