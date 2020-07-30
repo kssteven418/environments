6ec4e75abfa6aa9c6db4a26d4074fb176a5453fb
Sehoon Kim <kssteven418@gmail.com>
2020-07-30 11:55:58 +0900

    colorscheme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 cp .vimrc ~
 cp .bashrc ~
+cp .tmux.conf ~
+cp -rf colors ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe && ./install.sh
